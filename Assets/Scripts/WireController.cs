0ea514e910b391c6fb1838a60513b193fa311b5f
eph-eye-en <>
2022-10-03 20:34:17 +0100

    Make wires unpluggable and add more colours

--- Assets/Scripts/WireController.cs
+++ Assets/Scripts/WireController.cs
@@ -9,6 +9,7 @@ public class WireController : MonoBehaviour
     public Vector3 StartPos;
     public Vector3 EndPos;
     public float Height;
+    public bool Unplugged;
 
     private LineRenderer lineRenderer;
 
@@ -22,12 +23,21 @@ public class WireController : MonoBehaviour
         for(int i = 0; i < NumPoints; i++) {
             // NumPoints - 1 because the end points are inclusive
             float iScaled = (float)i / (NumPoints - 1);
+            float y = Unplugged
+                ? Mathf.Lerp(StartPos.y, EndPos.y, iScaled)
+                    + Mathf.Sin(iScaled * Mathf.PI / 2) * Height * 2
+                : Mathf.Lerp(StartPos.y, EndPos.y, iScaled)
+                    + Mathf.Sin(iScaled * Mathf.PI) * Height;
             lineRenderer.SetPosition(i, new Vector3(
                 Mathf.Lerp(StartPos.x, EndPos.x, iScaled),
-                Mathf.Lerp(StartPos.y, EndPos.y, iScaled)
-                    + Mathf.Sin(iScaled * Mathf.PI) * Height,
+                y,
                 Mathf.Lerp(StartPos.z, EndPos.z, iScaled)
             ));
         }
+    }
+
+    public void SetMaterial(Material m) {
+        //Start may not have assigned lineRenderer yet
+        GetComponent<LineRenderer>().materials = new Material[]{ m };
     }
 }
--- Assets/Scripts/WiresModule.cs
+++ Assets/Scripts/WiresModule.cs
@@ -1,55 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class WiresModule : MonoBehaviour
+public class WiresModule : ModuleController
 {
     public int LeftCount;
     public int RightCount;
-    public int WireCount;
+    public List<Material> WireMaterials;
+    [System.Serializable]
+    public struct WireConnection {
+        public int MaterialIndex;
+        public int Left;
+        public int Right;
+        public float Height;
+        public int CutOrder;
+    }
+    public List<WireConnection> WireConnections;
 
     [SerializeField]
     private Transform leftNodesOrigin;
     [SerializeField]
     private Transform rightNodesOrigin;
     [SerializeField]
-    private GameObject nodePrefab;
+    private Clickable nodePrefab;
     [SerializeField]
     private WireController wirePrefab;
 
-    private GameObject[] leftNodes;
-    private GameObject[] rightNodes;
-    private WireController[] wires;
+    private Clickable[] leftNodes;
+    private Clickable[] rightNodes;
+    private struct Wire {
+        public WireController Controller;
+        public int CutOrder;
+        public int Left;
+        public int Right;
+    }
+    private List<Wire> wires = new List<Wire>();
+    private int currCutOrderIndex;
 
     void Start()
     {
-        leftNodes = CreateNodes(leftNodesOrigin, LeftCount);
-        rightNodes = CreateNodes(rightNodesOrigin, RightCount);
-        wires = CreateWires(WireCount);
+        leftNodes = CreateNodes(leftNodesOrigin, LeftCount, false);
+        rightNodes = CreateNodes(rightNodesOrigin, RightCount, true);
+        foreach (var wc in WireConnections) {
+            var c = CreateWireController(WireMaterials[wc.MaterialIndex],
+                wc.Left, wc.Right, wc.Height);
+            wires.Add(new Wire {
+                Controller = c,
+                CutOrder = wc.CutOrder,
+                Left = wc.Left,
+                Right = wc.Right,
+            });
+        }
     }
 
-    GameObject[] CreateNodes(Transform origin, int count) {
-        GameObject[] nodes = new GameObject[count];
+    void UnplugNode(int n, bool isRight) {
+        foreach (var w in wires)
+        {
+            var c = w.Controller;
+            if(!c.Unplugged
+            && (isRight || w.Left == n)
+            && (!isRight || w.Right == n)) {
+                if(w.CutOrder == currCutOrderIndex) {
+                    c.Unplugged = true;
+                    if(!isRight) {
+                        var tmp = c.StartPos;
+                        c.StartPos = c.EndPos;
+                        c.EndPos = tmp;
+                    }
+                    c.RegeneratePoints();
+                }
+                else {
+                    OnFail.Invoke();
+                    return;
+                }
+            }
+        }
+    }
+
+    Clickable[] CreateNodes(Transform origin, int count, bool isRight) {
+        var nodes = new Clickable[count];
         for(int i = 0; i < count; i++) {
             nodes[i] = Instantiate(nodePrefab, origin);
             nodes[i].transform.localPosition
                 = new Vector3(0, 0, (i - (count - 1) / 2f) * 0.8f / count);
+            // Capture variable by value, not reference
+            int i_ = i;
+            nodes[i].OnMouseDownEvent.AddListener(
+                () => UnplugNode(i_, isRight));
         }
         return nodes;
     }
 
-    WireController[] CreateWires(int count) {
-        WireController[] ws = new WireController[count];
-        for(int i = 0; i < count; i++) {
-            GameObject start = leftNodes[Random.Range(0, leftNodes.Length)];
-            GameObject end = rightNodes[Random.Range(0, rightNodes.Length)];
-            WireController wire = Instantiate(wirePrefab, transform);
-            wire.transform.localPosition = Vector3.zero;
-            wire.transform.localRotation = Quaternion.identity;
-            wire.StartPos = start.transform.position;
-            wire.EndPos = end.transform.position;
-            wire.Height = Random.Range(0.05f, 0.25f);
-        }
-        return ws;
+    WireController CreateWireController(Material mat, int left, int right,
+    float height) {
+        var start = leftNodes[left];
+        var end = rightNodes[right];
+        WireController wire = Instantiate(wirePrefab, transform);
+        wire.transform.localPosition = Vector3.zero;
+        wire.transform.localRotation = Quaternion.identity;
+        wire.StartPos = start.transform.position;
+        wire.EndPos = end.transform.position;
+        wire.Height = height;
+        wire.Unplugged = false;
+        wire.SetMaterial(mat);
+        return wire;
     }
 }
